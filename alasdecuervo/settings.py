e673306fa3f56eb4533a958153ad03353b00210d
Manzanero <Github=91>
2018-11-04 15:06:40 +0100

    navbar

--- alasdecuervo/settings.py
+++ alasdecuervo/settings.py
@@ -23,8 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2gxdqe)n!$4p#!@wbpmlrcu-p2a@i-&7b23%#p8dstn)6$*r_1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = False
+DEBUG = True
+# DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'alasdecuervo.pythonanywhere.com']
 
