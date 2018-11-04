5bf410c2e5a4f5251d36cf303cc2df0ca40ef907
Manzanero <Github=91>
2018-11-04 11:02:23 +0100

    detalles

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import News
 
 
 def news_list(request):
-    news = News.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-
+    news = News.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/news_list.html', {'news': news})
+
+
+def news_detail(request, pk):
+    piece = get_object_or_404(News, pk=pk)
+    return render(request, 'blog/news_detail.html', {'piece': piece})
